b9bd2ddb5f010c18ddfe98dfe808754d119061fb
Aliaksei Skakun <skaliaksei@gmail.com>
2021-02-21 14:15:08 +0300

    feat: change slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
     $('.slider').slick({
         arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true
-    });
+      });
 });
